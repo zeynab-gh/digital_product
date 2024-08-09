cbc6e39b87dee5b7087cc7792964881baa87ad82
zeynab <computer.1818@yahoo.com>
2024-08-09 15:51:42 +0430

    models

--- digital_products/settings.py
+++ digital_products/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- products/models.py
+++ products/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
+    titel = models.CharField('title', max_length=50)
+    description = models.CharField('description', max_length=250)
+    avatar = models.ImageField(blank=True, upload_to='categories')
+    is_enable = models.BooleanField(default=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+
+class Product(models.Model):
+    category = models.ManyToManyField('Category', blank=True)
+    titel = models.CharField('title', max_length=50)
+    description = models.CharField('description', max_length=250)
+    avatar = models.ImageField(blank=True, upload_to='products/')
+    is_enable = models.BooleanField(default=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+
+class File(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    titel = models.CharField('title', max_length=50)
+    description = models.CharField('description', max_length=250)
+    is_enable = models.BooleanField(default=True)
+    file = models.FileField(upload_to='files')
+    create_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'files'
+        verbose_name = 'File'
+        verbose_name_plural = 'Files'
